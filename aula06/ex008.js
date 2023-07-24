c24b2f51aac8f8a319da9230cf1b396dca191984
cristianoloureno <137234520+cristianoloureno@users.noreply.github.com>
2023-07-24 09:00:16 -0300

    atualização js

---
+++ aula06/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`Velociadade é ${vel} Km/H!`)
+if (vel > 60) {
+    console.log('Ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre com segurança!')
---
+++ aula06/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Viver em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Vocé Brasileiro')
+} else {
+    console.log('você é Estrangeiro')
+}
